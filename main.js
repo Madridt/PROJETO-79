cef82178bcf257561b90069f1be1d0281d5d9511
Madridt <123021013+Madridt@users.noreply.github.com>
2023-04-30 13:58:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+menuListArray.sort();
+var item=document.getElementById("addItem").value;
+menuListArray.push(item);
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+htmldata=htmldata+imgtags+ menuListArray[i]+'<br>';
+document.getElementById("displayMenu").innerHTML =htmldata;
+
+
+menuListArray = ["Pizza Vegetariana",
+                 "Pizza de Frango",
+                 "Pizza Portuguesa",
+                 "Pizza Quatro Queijos",
+                 "Pizza Calabresa",
+                 "Pizza Extravaganza",];
+
+function getMenu(){
+var htmldata="";
+menuListArray.sort();
+for(var i=0; i<menuListArray.lenght;i++){
+    htmldata=htmldata+ menuListArray[i] + '<br>'
+}
+document.getElementById("displayMenu").innerHTML = htmldata;
+}
+
+function addItem(){
+var htmldata;
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata=""
+for(var i=0;i<menuListArray.lenght;i++){
+    htmldata=htmldata+imgtags+menuListArray[i]+ '<br>';
+}
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
+}
+
+function addTop(){
+    var item=document.getElementById("additem").value;
+    menuListArray.push(item);
+    addItem();
+
+}
